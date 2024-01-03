6f1b3a8194bb9e181d307b2545bc4f2db7af1a95
Rai-vaz <ray.vaz04@gmail.com>
2024-01-03 12:54:08 -0300

    Garantir autenticação para acessar o dashboard

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,21 +4,20 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import Authenticated from './Layouts/AuthenticatedLayout';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })    
-        let page = pages[`./Pages/${name}.jsx`] 
-        console.log(name)
-        if (name == 'Welcome') {
+    resolve: (name) =>  resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')).then((page) => {
+        console.log(page)
+        if (name == 'Welcome' || name == 'Auth/Login') {
             return page
         }
         page.default.layout = page.default.layout || (page => <Authenticated user='tt' children={page} />)
-        return page   
-    },
+        return page  
+    }),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
@@ -27,7 +26,10 @@ createInertiaApp({
     progress: {
         color: '#4B5563',
     },
+           
     
 });
+
+
 
 
