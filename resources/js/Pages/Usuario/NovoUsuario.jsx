f29783e2d562a55bc58a83f75c71ba7083052c1e
Rai-vaz <ray.vaz04@gmail.com>
2024-02-01 14:40:50 -0300

    Organização página de usuários

---
+++ resources/js/Pages/Usuario/NovoUsuario.jsx
@@ -0,0 +1,151 @@
+import { HeaderPage } from "@/Components/HeaderPage"
+import { useEffect } from 'react';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
+
+
+
+export default function NovoUsuario({role}) {
+
+    const { data, setData, post, processing, errors, reset } = useForm({
+        type: '',
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: '',
+    });
+
+    useEffect(() => {
+        return () => {
+            reset('password', 'password_confirmation');
+        };
+    }, []);
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        post(route('usuarios.criar'));
+    };
+    
+  
+
+    return (
+        <>
+            <Head title="Cadastro Usuário" />
+            <HeaderPage 
+                iconD="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+
+                title='Cadastro de usuário'
+
+                link='Listar usuários'
+
+                href={route('usuarios')}
+            />
+
+            <div className="w-[50%] p-5 mx-auto ">
+                
+                <form onSubmit={submit}>
+
+                    <div className='mt-4'>
+                        <InputLabel value='Tipo de usuário'/>
+                        <select 
+                            className='w-full dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300  dark:focus:border-indigo-600 dark:focus:ring-indigo-600 rounded-md shadow-sm'
+                            defaultValue={data.type}
+                            onChange={
+                                (e) => setData('type', e.target.value)
+                            }
+                        >
+                            <option value={''} hidden disabled>Selecione o tipo de usuário</option>
+                            {
+                                
+                                role.map((value, key) => (
+                                    <option value={key + 1} key={key}>{value.type}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+
+                    <div className='mt-4'>
+                        <InputLabel htmlFor="name" value="Name" />
+
+                        <TextInput
+                            id="name"
+                            name="name"
+                            value={data.name}
+                            className="block w-full"
+                            autoComplete="name"
+                            isFocused={true}
+                            onChange={(e) => setData('name', e.target.value)}
+                            required
+                        />
+
+                        <InputError message={errors.name} className="mt-2" />
+                    </div>
+
+                    <div className="mt-4">
+                        <InputLabel htmlFor="email" value="Email" />
+
+                        <TextInput
+                            id="email"
+                            type="email"
+                            name="email"
+                            value={data.email}
+                            className="block w-full"
+                            autoComplete="username"
+                            onChange={(e) => setData('email', e.target.value)}
+                            required
+                        />
+
+                        <InputError message={errors.email} className="mt-2" />
+                    </div>
+
+                    <div className="mt-4">
+                        <InputLabel htmlFor="password" value="Password" />
+
+                        <TextInput
+                            id="password"
+                            type="password"
+                            name="password"
+                            value={data.password}
+                            className="block w-full"
+                            autoComplete="new-password"
+                            onChange={(e) => setData('password', e.target.value)}
+                            required
+                        />
+
+                        <InputError message={errors.password} className="mt-2" />
+                    </div>
+
+                    <div className="mt-4">
+                        <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+
+                        <TextInput
+                            id="password_confirmation"
+                            type="password"
+                            name="password_confirmation"
+                            value={data.password_confirmation}
+                            className="block w-full"
+                            autoComplete="new-password"
+                            onChange={(e) => setData('password_confirmation', e.target.value)}
+                            required
+                        />
+
+                        <InputError message={errors.password_confirmation} className="mt-2" />
+                    </div>
+
+                    <div className="flex items-center justify-end mt-4">
+                        <PrimaryButton className='ms-4' disabled={processing}>
+                            Cadastrar
+                        </PrimaryButton>
+                    </div>
+
+                </form>
+            
+            </div>
+        </>
+        
+    )
+}
